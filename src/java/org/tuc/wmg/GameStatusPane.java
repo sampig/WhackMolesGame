a54e23c905d0e90f2e062d8ff3153d9ea9ecb2d1
sampig <zhuchenf@gmail.com>
2015-07-07 16:32:15 +0200

    source_java

--- src/java/org/tuc/wmg/GameStatusPane.java
+++ src/java/org/tuc/wmg/GameStatusPane.java
@@ -1,9 +1,12 @@
 package org.tuc.wmg;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 
+import javax.swing.BorderFactory;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
 public class GameStatusPane extends JPanel {
@@ -11,19 +14,27 @@ public class GameStatusPane extends JPanel {
     private static final long serialVersionUID = 1935065644500091308L;
 
     private JLabel serialInfo;
-    private JPanel gamePane;
+    private JScrollPane serialPane, gamePane, infoPane;
     private JTextArea infoTextArea;
 
     public GameStatusPane(ServerUI server) {
         serialInfo = new JLabel();
         serialInfo.setText(server.getSource());
-        gamePane = new JPanel();
+        serialPane = new JScrollPane(serialInfo);
+        gamePane = new JScrollPane();
         infoTextArea = new JTextArea();
         infoTextArea.setRows(10);
+        infoPane = new JScrollPane(infoTextArea);
+        infoPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        infoPane.setPreferredSize(new Dimension(350, 300));
+        infoPane.setBorder(BorderFactory.createCompoundBorder(
+                BorderFactory.createCompoundBorder(
+                        BorderFactory.createTitledBorder("Information:"),
+                        BorderFactory.createEmptyBorder(1, 1, 1, 1)), infoPane.getBorder()));
         setLayout(new BorderLayout());
-        add(serialInfo, BorderLayout.NORTH);
+        add(serialPane, BorderLayout.NORTH);
         add(gamePane, BorderLayout.CENTER);
-        add(infoTextArea, BorderLayout.SOUTH);
+        add(infoPane, BorderLayout.SOUTH);
     }
 
     public void updateSerialInfo(String text) {
