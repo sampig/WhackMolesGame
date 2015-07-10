877fc0f00adf43b96e5714c2dc12515932489056
Chenfeng ZHU <czh14@tu-clausthal.de>
2015-07-10 16:24:03 +0200

    source_java

--- src/java/org/tuc/wmg/GameThread.java
+++ src/java/org/tuc/wmg/GameThread.java
@@ -15,196 +15,200 @@ import net.tinyos.message.MoteIF;
  */
 public class GameThread implements Runnable, MessageListener {
 
-    public enum GameStat {
-        CONFIGURE, RUNNING, OVER
-    }
+	public enum GameStat {
+		CONFIGURE, RUNNING, OVER
+	}
 
-    public enum TimerType {
-        CONFIG, RUNNING
-    }
+	public enum TimerType {
+		CONFIG, RUNNING
+	}
 
-    public final static int TIMEOUT_WAITING = 5;
+	public final static int TIMEOUT_WAITING = 5;
 
-    private ServerUI server;
-    private GameLevel level = GameLevel.LEVEL_BEGINNER;
-    private int totalTimes = 10;
-    private double timeoutMole = 5;
-    private double timeoutServer = 5;
-    private int numMoles = 4;
-    private List<Integer> listMoles = new ArrayList<Integer>(0);
+	private ServerUI server;
+	private GameLevel level = GameLevel.LEVEL_BEGINNER;
+	private int totalTimes = 10;
+	private double timeoutMole = 5;
+	private double timeoutServer = 5;
+	private int numMoles = 4;
+	private List<Integer> listMoles = new ArrayList<Integer>(0);
 
-    private TimerThread timerWait;
+	private TimerThread timerWait;
 
-    private GameStat stat = GameStat.CONFIGURE;
+	private GameStat stat = GameStat.CONFIGURE;
 
-    private int currentTimes = 1;
-    private int hitTimes = 0;
+	private int currentTimes = 1;
+	private int hitTimes = 0;
 
-    private MoteIF moteIF;
+	private MoteIF moteIF;
 
-    public GameThread(ServerUI server, MoteIF moteIF) {
-        this.server = server;
-        level = server.getLevel();
-        totalTimes = level.getTotalTimes();
-        timeoutMole = level.getTimeoutMole();
-        timeoutServer = level.getTimeoutServer();
-        numMoles = level.getNumMoles();
-        this.moteIF = moteIF;
-        this.moteIF.registerListener(new GameMsg(), this);
-    }
+	public GameThread(ServerUI server, MoteIF moteIF) {
+		this.server = server;
+		level = server.getLevel();
+		totalTimes = level.getTotalTimes();
+		timeoutMole = level.getTimeoutMole();
+		timeoutServer = level.getTimeoutServer();
+		numMoles = level.getNumMoles();
+		this.moteIF = moteIF;
+		this.moteIF.registerListener(new GameMsg(), this);
+		this.init();
+	}
 
-    @Override
-    public void run() {
-        if (stat == GameStat.CONFIGURE) {
-            this.sendConfiguration();
-        } else if (stat == GameStat.RUNNING) {
-            ;
-        } else if (stat == GameStat.OVER) {
-            ;
-        }
-    }
+	public void init() {
+		stat = GameStat.CONFIGURE;
+		currentTimes = 1;
+		hitTimes = 0;
+	}
 
-    @Override
-    public void messageReceived(int to, Message message) {
-        GameMsg msg = (GameMsg) message;
-        int source = message.getSerialPacket().get_header_src();
-        int type = msg.get_type();
-        int data = msg.get_data();
-        String text = "(Msg received from Mole." + source + ": Type=" + type + ", Data="
-                + data + ".) ";
-        if (type == 0x11 && listMoles.size() < numMoles) { // ACK: ready
-            if (!listMoles.contains(source)) {
-                text = "Mole." + source + " is ready. " + text;
-                server.getStatusPane().appendInfo(text);
-                listMoles.add(source);
-            }
-            if (listMoles.size() >= numMoles) {
-                stat = GameStat.RUNNING;
-                server.getStatusPane().appendInfo("Game starts.");
-                if (timerWait != null && timerWait.isAlive()) {
-                    try {
-                        timerWait.interrupt();
-                    } catch (Exception e) {
-                    }
-                }
-                this.sendMoleID();
-            }
-            return;
-        } else if (type == 0x13) { // ACK: myTurn
-            server.getStatusPane().appendInfo("Mole." + source + " came out. " + text);
-            return;
-        } else if (type == 0x22) { // Result: 0-miss;1-hit
-            this.sendStatReply(source);
-            boolean stat = (data == 0x01);
-            text = "Mole." + source + " stat: " + (stat ? "hit" : "miss") + ". " + text;
-            server.getStatusPane().appendInfo(text);
-            if (stat) {
-                hitTimes++;
-            }
-        }
-        // server.getStatusPane().appendInfo(text);
-        if (type == 0x22 && currentTimes <= totalTimes) {
-            this.sendMoleID();
-            currentTimes++;
-        } else {
-            this.sendGameOver();
-            return;
-        }
-    }
+	@Override
+	public void run() {
+		if (stat == GameStat.CONFIGURE) {
+			this.sendConfiguration();
+		} else if (stat == GameStat.RUNNING) {
+			;
+		} else if (stat == GameStat.OVER) {
+			;
+		}
+	}
 
-    public void sendConfiguration() {
-        listMoles.clear();
-        GameMsg msg = new GameMsg();
-        try {
-            msg.set_type(0x01);
-            msg.set_data((int) timeoutMole);
-            moteIF.send(MoteIF.TOS_BCAST_ADDR, msg);
-            server.getStatusPane().appendInfo("Configurating... Wait...");
-            server.getStatusPane().appendInfo("Game Level: " + level);
-            timerWait = new TimerThread(TimerType.CONFIG);
-            timerWait.start();
-        } catch (Exception ioexc) {
-        }
-    }
+	@Override
+	public void messageReceived(int to, Message message) {
+		if (stat == GameStat.OVER) {
+			return;
+		}
+		GameMsg msg = (GameMsg) message;
+		int source = message.getSerialPacket().get_header_src();
+		int type = msg.get_type();
+		int data = msg.get_data();
+		String text = "(Msg received from Mole." + source + ": Type=" + type + ", Data=" + data + ".) ";
+		if (type == 0x11 && listMoles.size() < numMoles) { // ACK: ready
+			if (!listMoles.contains(source)) {
+				text = "Mole." + source + " is ready. " + text;
+				server.getStatusPane().appendInfo(text);
+				listMoles.add(source);
+			}
+			if (listMoles.size() >= numMoles) {
+				stat = GameStat.RUNNING;
+				server.getStatusPane().appendInfo("Game starts.");
+				this.sendMoleID();
+			}
+			return;
+		} else if (type == 0x13) { // ACK: myTurn
+			server.getStatusPane().appendInfo("Mole." + source + " came out. " + text);
+			return;
+		} else if (type == 0x22) { // Result: 0-miss;1-hit
+			this.sendStatReply(source);
+			boolean stat = (data == 0x01);
+			text = "Mole." + source + " stat: " + (stat ? "hit" : "miss") + ". " + text;
+			server.getStatusPane().appendInfo(text);
+			if (stat) {
+				hitTimes++;
+			}
+			this.sendMoleID();
+			currentTimes++;
+		}
+		if (currentTimes >= totalTimes) {
+			this.sendGameOver();
+			return;
+		}
+	}
 
-    /**
-     * Choose one of the moles.
-     */
-    public void sendMoleID() {
-        try {
-            Thread.sleep((long) (timeoutServer * 1000));
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        GameMsg msg = new GameMsg();
-        Random r = new Random(System.currentTimeMillis());
-        int id = r.nextInt(numMoles);
-        try {
-            msg.set_type(0x21);
-            msg.set_data(listMoles.get(id));
-            moteIF.send(MoteIF.TOS_BCAST_ADDR, msg);
-            server.getStatusPane().appendInfo("It is the turn of Mole." + listMoles.get(id));
-            server.getStatusPane().freshGamepane(id);
-        } catch (Exception ioexc) {
-        }
-    }
+	public void sendConfiguration() {
+		listMoles.clear();
+		GameMsg msg = new GameMsg();
+		try {
+			msg.set_type(0x01);
+			msg.set_data((int) timeoutMole);
+			moteIF.send(MoteIF.TOS_BCAST_ADDR, msg);
+			server.getStatusPane().appendInfo("Configurating... Wait...");
+			server.getStatusPane().appendInfo("Game Level: " + level);
+			timerWait = new TimerThread(TimerType.CONFIG);
+			timerWait.start();
+		} catch (Exception ioexc) {
+		}
+	}
 
-    public void sendStatReply(int source) {
-        GameMsg msg = new GameMsg();
-        try {
-            msg.set_type(0x14);
-            msg.set_data(source);
-            moteIF.send(source, msg);
-        } catch (Exception ioexc) {
-        }
-    }
+	/**
+	 * Choose one of the moles.
+	 */
+	public void sendMoleID() {
+		try {
+			Thread.sleep((long) (timeoutServer * 1000));
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		GameMsg msg = new GameMsg();
+		Random r = new Random(System.currentTimeMillis());
+		int id = r.nextInt(numMoles);
+		try {
+			msg.set_type(0x21);
+			msg.set_data(listMoles.get(id));
+			moteIF.send(MoteIF.TOS_BCAST_ADDR, msg);
+			server.getStatusPane().appendInfo("It is the turn of Mole." + listMoles.get(id));
+			server.getStatusPane().freshGamepane(id);
+		} catch (Exception ioexc) {
+		}
+	}
 
-    public void sendGameOver() {
-        GameMsg msg = new GameMsg();
-        double result = (hitTimes * 1.0 / totalTimes);
-        StringBuffer text = new StringBuffer();
-        text.append("*********************\n");
-        text.append("***** Game Over *****\n");
-        text.append(server.getPlayerName() + "\'s result: \n");
-        text.append("Game Level: " + server.getLevel() + ".\n");
-        text.append("Total Times: " + totalTimes + "\n");
-        text.append("Hit Times: " + hitTimes + "\n");
-        text.append("Result: " + result + "\n");
-        try {
-            msg.set_type(0xFF);
-            msg.set_data(0x00);
-            moteIF.send(MoteIF.TOS_BCAST_ADDR, msg);
-            server.getStatusPane().appendInfo(text.toString());
-        } catch (Exception ioexc) {
-        }
-        server.getRankPane().insertNewResult(server.getPlayerName(), result, hitTimes,
-                totalTimes);
-    }
+	public void sendStatReply(int source) {
+		GameMsg msg = new GameMsg();
+		try {
+			msg.set_type(0x14);
+			msg.set_data(source);
+			moteIF.send(source, msg);
+		} catch (Exception ioexc) {
+		}
+	}
 
-    public class TimerThread extends Thread {
+	public void sendGameOver() {
+		GameMsg msg = new GameMsg();
+		double result = (hitTimes * 1.0 / totalTimes);
+		StringBuffer text = new StringBuffer();
+		text.append("*********************\n");
+		text.append("***** Game Over *****\n");
+		text.append(server.getPlayerName() + "\'s result: \n");
+		text.append("Game Level: " + server.getLevel() + ".\n");
+		text.append("Total Times: " + totalTimes + "\n");
+		text.append("Hit Times: " + hitTimes + "\n");
+		text.append("Result: " + result + "\n");
+		try {
+			msg.set_type(0xFF);
+			msg.set_data(0x00);
+			moteIF.send(MoteIF.TOS_BCAST_ADDR, msg);
+			server.getStatusPane().appendInfo(text.toString());
+		} catch (Exception ioexc) {
+		}
+		server.getRankPane().insertNewResult(server.getPlayerName(), result, hitTimes, totalTimes);
+		stat = GameStat.OVER;
+		server.stop();
+	}
 
-        private TimerType type = TimerType.CONFIG;
+	public class TimerThread extends Thread {
 
-        public TimerThread(TimerType type) {
-            this.type = type;
-        }
+		private TimerType type = TimerType.CONFIG;
 
-        public TimerType getType() {
-            return type;
-        }
+		public TimerThread(TimerType type) {
+			this.type = type;
+		}
 
-        public void run() {
-            if (type == TimerType.CONFIG) {
-                try {
-                    Thread.sleep((long) (TIMEOUT_WAITING * 1000));
-                    server.getStatusPane().appendInfo("No repsponse. Send config again.");
-                    sendConfiguration();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
+		public TimerType getType() {
+			return type;
+		}
 
-    }
+		public void run() {
+			if (type == TimerType.CONFIG) {
+				try {
+					Thread.sleep((long) (TIMEOUT_WAITING * 1000));
+					if (stat == GameStat.CONFIGURE) {
+						server.getStatusPane().appendInfo("No repsponse. Send config again.");
+						sendConfiguration();
+					}
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+	}
 
 }
--- src/java/org/tuc/wmg/ServerUI.java
+++ src/java/org/tuc/wmg/ServerUI.java
@@ -37,198 +37,202 @@ import org.tuc.wmg.menu.OptionsDialog;
  */
 public class ServerUI extends JPanel {
 
-    private static final long serialVersionUID = -6911094015780609780L;
+	private static final long serialVersionUID = -6911094015780609780L;
 
-    private String title = "Whack Moles Game";
-    private boolean isRunning = false;
+	private String title = "Whack Moles Game";
+	private boolean isRunning = false;
 
-    private JSplitPane mainPane;
-    private GameRankPane rankPane;
-    private GameStatusPane statusPane;
-    private JLabel statusBar;
+	private JSplitPane mainPane;
+	private GameRankPane rankPane;
+	private GameStatusPane statusPane;
+	private JLabel statusBar;
 
-    private GameLevel level = GameLevel.LEVEL_BEGINNER;
+	private GameLevel level = GameLevel.LEVEL_BEGINNER;
 
-    private GameThread game;
-    private String source = "serial@/dev/ttyUSB0:115200";
-    private String playerName;
+	private GameThread game;
+	private String source = "serial@/dev/ttyUSB0:115200";
+	private String playerName;
 
-    public ServerUI() {
-        rankPane = new GameRankPane(this);
-        statusPane = new GameStatusPane(this);
-        mainPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, rankPane, statusPane);
-        mainPane.setOneTouchExpandable(true);
-        mainPane.setDividerLocation(200);
-        mainPane.setDividerSize(6);
-        mainPane.setBorder(null);
-        statusBar = createStatusBar();
-        setLayout(new BorderLayout());
-        add(mainPane, BorderLayout.CENTER);
-        add(statusBar, BorderLayout.SOUTH);
-        installToolBar();
-    }
+	private PhoenixSource phoenixSource;
 
-    public JFrame createFrame() {
-        JFrame frame = new JFrame();
-        frame.getContentPane().add(this);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setJMenuBar(new ControlMenuBar(this));
-        frame.setSize(800, 600);
-        updateTitle();
-        return frame;
-    }
+	public ServerUI() {
+		rankPane = new GameRankPane(this);
+		statusPane = new GameStatusPane(this);
+		mainPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, rankPane, statusPane);
+		mainPane.setOneTouchExpandable(true);
+		mainPane.setDividerLocation(200);
+		mainPane.setDividerSize(6);
+		mainPane.setBorder(null);
+		statusBar = createStatusBar();
+		setLayout(new BorderLayout());
+		add(mainPane, BorderLayout.CENTER);
+		add(statusBar, BorderLayout.SOUTH);
+		installToolBar();
+	}
 
-    private void installToolBar() {
-        add(new ControlToolBar(this, JToolBar.HORIZONTAL), BorderLayout.NORTH);
-    }
+	public JFrame createFrame() {
+		JFrame frame = new JFrame();
+		frame.getContentPane().add(this);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setJMenuBar(new ControlMenuBar(this));
+		frame.setSize(800, 600);
+		updateTitle();
+		return frame;
+	}
 
-    private JLabel createStatusBar() {
-        JLabel statusBar = new JLabel("ready");
-        statusBar.setBorder(BorderFactory.createEmptyBorder(2, 4, 2, 4));
-        return statusBar;
-    }
+	private void installToolBar() {
+		add(new ControlToolBar(this, JToolBar.HORIZONTAL), BorderLayout.NORTH);
+	}
 
-    /**
-     * Update the title of the window.
-     */
-    public void updateTitle() {
-        JFrame frame = (JFrame) SwingUtilities.windowForComponent(this);
-        if (frame != null) {
-            title = "Whack Moles Game";
-            if (isRunning) {
-                title += " - running";
-            }
-            frame.setTitle(title);
-        }
-    }
+	private JLabel createStatusBar() {
+		JLabel statusBar = new JLabel("ready");
+		statusBar.setBorder(BorderFactory.createEmptyBorder(2, 4, 2, 4));
+		return statusBar;
+	}
 
-    /**
-     * Update the text of the status bar.
-     * 
-     * @param msg
-     */
-    public void updateStatusBar(String msg) {
-        statusBar.setText(msg);
-    }
+	/**
+	 * Update the title of the window.
+	 */
+	public void updateTitle() {
+		JFrame frame = (JFrame) SwingUtilities.windowForComponent(this);
+		if (frame != null) {
+			title = "Whack Moles Game";
+			if (isRunning) {
+				title += " - running";
+			}
+			frame.setTitle(title);
+		}
+	}
 
-    public Action bind(String name, final Action action) {
-        return bind(name, action, null);
-    }
+	/**
+	 * Update the text of the status bar.
+	 * 
+	 * @param msg
+	 */
+	public void updateStatusBar(String msg) {
+		statusBar.setText(msg);
+	}
 
-    public Action bind(String name, final Action action, Icon icon) {
-        AbstractAction newAction = new AbstractAction(name, icon) {
-            private static final long serialVersionUID = 6738094277262331128L;
+	public Action bind(String name, final Action action) {
+		return bind(name, action, null);
+	}
 
-            public void actionPerformed(ActionEvent e) {
-                action.actionPerformed(new ActionEvent(mainPane, e.getID(), e
-                        .getActionCommand()));
-            }
-        };
-        newAction
-                .putValue(Action.SHORT_DESCRIPTION, action.getValue(Action.SHORT_DESCRIPTION));
-        return newAction;
-    }
+	public Action bind(String name, final Action action, Icon icon) {
+		AbstractAction newAction = new AbstractAction(name, icon) {
+			private static final long serialVersionUID = 6738094277262331128L;
 
-    public void start() {
-        isRunning = true;
-        updateTitle();
-        updateStatusBar("Running");
-        PhoenixSource phoenix;
-        if (source == null) {
-            phoenix = BuildSource.makePhoenix(PrintStreamMessenger.err);
-        } else {
-            phoenix = BuildSource.makePhoenix(source, PrintStreamMessenger.err);
-        }
-        MoteIF mif = new MoteIF(phoenix);
-        game = new GameThread(this, mif);
-        Thread thread = new Thread(game);
-        thread.start();
-    }
+			public void actionPerformed(ActionEvent e) {
+				action.actionPerformed(new ActionEvent(mainPane, e.getID(), e.getActionCommand()));
+			}
+		};
+		newAction.putValue(Action.SHORT_DESCRIPTION, action.getValue(Action.SHORT_DESCRIPTION));
+		return newAction;
+	}
 
-    public void chooseLevel(GameLevel level) {
-        this.level = level;
-    }
+	public void start() {
+		isRunning = true;
+		updateTitle();
+		updateStatusBar("Running");
+		if (source == null) {
+			phoenixSource = BuildSource.makePhoenix(PrintStreamMessenger.err);
+		} else {
+			phoenixSource = BuildSource.makePhoenix(source, PrintStreamMessenger.err);
+		}
+		MoteIF mif = new MoteIF(phoenixSource);
+		game = new GameThread(this, mif);
+		Thread thread = new Thread(game);
+		thread.start();
+	}
 
-    public void stop() {
-        if (game != null) {
-            Thread thread = new Thread(game);
-            thread.interrupt();
-            // Thread.currentThread().interrupt();
-        }
-    }
+	public void chooseLevel(GameLevel level) {
+		this.level = level;
+	}
 
-    public void options() {
-        JFrame frame = (JFrame) SwingUtilities.windowForComponent(this);
-        if (frame != null) {
-            OptionsDialog options = new OptionsDialog(this, frame);
-            options.setModal(true);
-            int x = frame.getX() + (frame.getWidth() - options.getWidth()) / 2;
-            int y = frame.getY() + (frame.getHeight() - options.getHeight()) / 2;
-            options.setLocation(x, y);
-            options.setVisible(true);
-        }
-    }
+	public void stop() {
+		if (phoenixSource != null) {
+			phoenixSource.shutdown();
+		}
+		if (game != null) {
+			Thread thread = new Thread(game);
+			thread.interrupt();
+			game = null;
+			System.gc();
+			// Thread.currentThread().interrupt();
+		}
+	}
 
-    public void about() {
-        JFrame frame = (JFrame) SwingUtilities.windowForComponent(this);
-        if (frame != null) {
-            AboutDialog about = new AboutDialog(frame);
-            about.setModal(true);
-            int x = frame.getX() + (frame.getWidth() - about.getWidth()) / 2;
-            int y = frame.getY() + (frame.getHeight() - about.getHeight()) / 2;
-            about.setLocation(x, y);
-            about.setVisible(true);
-        }
-    }
+	public void options() {
+		JFrame frame = (JFrame) SwingUtilities.windowForComponent(this);
+		if (frame != null) {
+			OptionsDialog options = new OptionsDialog(this, frame);
+			options.setModal(true);
+			int x = frame.getX() + (frame.getWidth() - options.getWidth()) / 2;
+			int y = frame.getY() + (frame.getHeight() - options.getHeight()) / 2;
+			options.setLocation(x, y);
+			options.setVisible(true);
+		}
+	}
 
-    public void exit() {
-        JFrame frame = (JFrame) SwingUtilities.windowForComponent(this);
-        if (frame != null) {
-            frame.dispose();
-        }
-    }
+	public void about() {
+		JFrame frame = (JFrame) SwingUtilities.windowForComponent(this);
+		if (frame != null) {
+			AboutDialog about = new AboutDialog(frame);
+			about.setModal(true);
+			int x = frame.getX() + (frame.getWidth() - about.getWidth()) / 2;
+			int y = frame.getY() + (frame.getHeight() - about.getHeight()) / 2;
+			about.setLocation(x, y);
+			about.setVisible(true);
+		}
+	}
 
-    public void setLevel(GameLevel level) {
-        this.level = level;
-    }
+	public void exit() {
+		JFrame frame = (JFrame) SwingUtilities.windowForComponent(this);
+		if (frame != null) {
+			frame.dispose();
+		}
+	}
 
-    public GameLevel getLevel() {
-        return level;
-    }
+	public void setLevel(GameLevel level) {
+		this.level = level;
+	}
 
-    public void setSource(String source) {
-        this.source = source;
-    }
+	public GameLevel getLevel() {
+		return level;
+	}
 
-    public String getSource() {
-        return source;
-    }
+	public void setSource(String source) {
+		this.source = source;
+	}
 
-    public String getPlayerName() {
-        return playerName;
-    }
+	public String getSource() {
+		return source;
+	}
 
-    public void setPlayerName(String playerName) {
-        this.playerName = playerName;
-    }
+	public String getPlayerName() {
+		return playerName;
+	}
 
-    public GameRankPane getRankPane() {
-        return rankPane;
-    }
+	public void setPlayerName(String playerName) {
+		this.playerName = playerName;
+	}
 
-    public GameStatusPane getStatusPane() {
-        return statusPane;
-    }
+	public GameRankPane getRankPane() {
+		return rankPane;
+	}
 
-    public static void main(String... strings) {
-        try {
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        } catch (Exception e1) {
-            e1.printStackTrace();
-        }
-        ServerUI server = new ServerUI();
-        server.createFrame().setVisible(true);
-        ;
-    }
+	public GameStatusPane getStatusPane() {
+		return statusPane;
+	}
+
+	public static void main(String... strings) {
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}
+		ServerUI server = new ServerUI();
+		server.createFrame().setVisible(true);
+		;
+	}
 
 }
